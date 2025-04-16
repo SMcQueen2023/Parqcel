956b994fe30a7cd433d9858189342db79c327863
Scott McQueen <138873952+SMcQueen2023@users.noreply.github.com>
2025-04-15 21:58:44 -0500

    Created init file.

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+# app/__init__.py
+
+"""
+Parqcel package
+This is the main package for Parqcel, a Parquet file viewer and editor.
+"""
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from app.viewer import ParquetViewer
 from PyQt5.QtWidgets import QApplication
 import sys
+import os
+
+# Ensure the parent directory of 'app' is in the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
