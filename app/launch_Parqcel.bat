f826bb4b78628160351074b5a258b2177022048e
Scott McQueen <138873952+SMcQueen2023@users.noreply.github.com>
2025-04-15 23:54:14 -0500

    Added bacth file to run app.

---
+++ app/launch_Parqcel.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\scott\OneDrive\Documents\GitHub\Parqcel\app"  # Change this to where your script is
+.\venv\Scripts\activate  # Activate the virtual environment
+python main.py  # Run your main script
+pause  # Keeps the terminal open after execution
